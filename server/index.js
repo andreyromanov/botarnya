b0c37df17b37fd5976152dfb62c3a753b2a18442
andreyromanov <andreygorogogo@gmail.com>
2020-03-31 15:47:45 +0300

    telegram bot init

--- server/index.js
+++ server/index.js
@@ -11,3 +11,12 @@ app.use(bodyParser.json());
 app.listen(PORT, function(){
   console.log('Server is running on Port:',PORT);
 });
+
+const Telegraf = require('telegraf')
+
+const bot = new Telegraf('1019430733:AAFOUpViOsa25fU2o59_PfROusJz0iMZOlI')
+bot.start((ctx) => ctx.reply('Welcome!'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
