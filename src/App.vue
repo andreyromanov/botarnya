32c0fcaf2cd3c32cfd5941cf44a31ba82aaf63ac
andreyromanov <andreygorogogo@gmail.com>
2020-04-04 19:48:46 +0300

    display dialogs

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/posts">Posts</router-link> |
+      <router-link to="/posts">Dialogs</router-link> |
       <router-link to="/create">Create</router-link>
     </div>
 
--- src/components/IndexComponent.vue
+++ src/components/IndexComponent.vue
@@ -1,18 +1,47 @@
  <template>
-  <div class="row justify-content-center">
-      <div class="col-md-8">
-          <div class="card card-default">
-              <div class="card-header">Index Component</div>
+  <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card card-default">
+                <div class="card-header">Index Component</div>
 
-              <div class="card-body">
-                  I'm an Index component.
-              </div>
-          </div>
-      </div>
+                <div class="card-body">
+                    <table class="table table-hover">
+                      <thead>
+                      <tr>
+                        <th>Question</th>
+                        <th>Answer</th>
+                        <th colspan="2">Actions</th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                          <tr v-for="dialog in dialogs" :key="dialog.id">
+                            <td>{{ dialog.req }}</td>
+                            <td>{{ dialog.res }}</td>
+                            <td class="text-right"><router-link :to="{name: 'edit', params: { id: dialog.id }}" class="btn btn-primary">Edit</router-link></td>
+                            <td><button class="btn btn-danger">Delete</button></td>
+                          </tr>
+                      </tbody>
+                  </table>
+                </div>
+            </div>
+        </div>
+    </div>
   </div>
  </template>
 
  <script>
   export default {
+      data() {
+        return {
+          dialogs: []
+        }
+      },
+      created() {
+      let uri = 'http://localhost:4000/dialogs';
+      this.axios.get(uri).then(response => {
+        this.dialogs = response.data;
+      });
+    }
   }
- </script>
+</script>
